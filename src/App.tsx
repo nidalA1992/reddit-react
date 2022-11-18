946c66c8eae892189f2d5749efe91e64ef212372
Nidal <nidalakhmedjanov@yandex.ru>
2022-11-18 22:56:13 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './main.global.css';
 import { hot } from 'react-hot-loader/root';
-import { Layout } from './shared/Layout/Layout';
-import { Header } from './shared/Header';
-import { Content } from './shared/Content';
-import { CardsList } from './shared/CardsList';
 import { useRequestToken } from './hooks/useRequestToken';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import { Post } from './shared/Post';
 import MainPage from './shared/pages/MainPage';
 import NotFoundPage from './shared/pages/NotFoundPage';
-
 
 function AppComponent() {
 
